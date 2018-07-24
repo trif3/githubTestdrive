9c4348cb9b2b7eef7aa409ff8e43c287845c6436
Trifon <tzafiridis@mzt.gr>
2018-07-24 10:38:23 +0300

    props commit

--- GithubTestdrive/GithubTestdrive/StoreEntities/Customer.cs
+++ GithubTestdrive/GithubTestdrive/StoreEntities/Customer.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GithubTestdrive
+﻿namespace GithubTestdrive.StoreEntities
 {
     public class Customer
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
--- GithubTestdrive/GithubTestdrive/StoreEntities/Order.cs
+++ GithubTestdrive/GithubTestdrive/StoreEntities/Order.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GithubTestdrive
+﻿namespace GithubTestdrive.StoreEntities
 {
     public class Order
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
     }
 }
