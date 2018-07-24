1651a13b15b923e36ba4a49d26b9846d266f4eaf
Trifon <tzafiridis@mzt.gr>
2018-07-24 10:42:37 +0300

    acc drive

---
+++ GithubTestdrive/GithubTestdrive/AccEntities/Invoice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubTestdrive.AccEntities
+{
+    public class Invoice
+    {
+        public int Id { get; set; }
+        public string Document { get; set; }
+    }
+}
