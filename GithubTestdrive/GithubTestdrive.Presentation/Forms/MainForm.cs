befee91e8b6fe2727b649a4ec510fd990de90b62
cnikolaidis <cnikolaidis>
2018-07-24 13:20:17 +0300

    Presentation project in cnTesting Branch

---
+++ GithubTestdrive/GithubTestdrive.Presentation/Forms/MainForm.cs
@@ -0,0 +1,35 @@
+﻿namespace GithubTestdrive.Presentation.Forms
+{
+    #region Usings
+    using System.Windows.Forms;
+    using System;
+    #endregion
+
+    public partial class MainForm : Form
+    {
+        #region Constructor
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Event Methods
+        protected void ButtonClicked(object sender, EventArgs e)
+        {
+            Button btnSender = (sender as Button);
+            if (btnSender != null)
+            {
+                switch (btnSender.Name)
+                {
+                    case (""):
+                        break;
+                    default:
+                        MessageBox.Show("Error", "Not a valid event.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        break;
+                }
+            }
+        }
+        #endregion
+    }
+}
---
+++ GithubTestdrive/GithubTestdrive.Presentation/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace GithubTestdrive.Presentation
+{
+    #region Usings
+    using GithubTestdrive.Presentation.Forms;
+    using System.Windows.Forms;
+    #endregion
+
+    static class Program
+    {
+        static void Main()
+        {
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.EnableVisualStyles();
+            Application.Run(new MainForm());
+        }
+    }
+}
